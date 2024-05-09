00fe247aa8fe049b78e55f2d17b4268e9b086808
aylahoffman <aahoffman@dons.usfca.edu>
2024-05-09 09:32:03 -0700

    6th

--- jesus9.js
+++ jesus9.js
@@ -1,5 +1,7 @@
 var img1;
 
+let p1= 'Love is rising\nNo resisting\nCheeks are flushing\nNow youre living'
+
 function preload () {
   img1 = loadImage("jesus.jpg");
 }
@@ -18,4 +20,12 @@ function draw() {
   
       ellipse(mouseX, mouseY, 33, 33);
       fill('#FFC107')
+      if(mouseX > 100 && mouseIsPressed)
+      text(p1,50,20)
+  textSize(30);
+ textFont(`Courier`);
+ fill('#FFC107');
+ textAlign(LEFT,TOP);
+ 
+     ellipse(mouseX, mouseY, 33, 33);
 }
